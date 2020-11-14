a925bb792b5e96ed0b69c216d4b37acbfdd01cf5
ermiry <erick_salas@ermiry.com>
2020-11-13 19:26:32 -0600

    Refactored version methods to use cerver log

--- src/version.c
+++ src/version.c
@@ -1,26 +1,26 @@
-#include <stdio.h>
+#include <cerver/utils/log.h>
 
 #include "version.h"
 
 // print full things version information
 void things_version_print_full (void) {
 
-	printf ("\n\nTiny Things Version: %s\n", THINGS_VERSION_NAME);
-	printf ("Release Date & time: %s - %s\n", THINGS_VERSION_DATE, THINGS_VERSION_TIME);
-	printf ("Author: %s\n\n", THINGS_VERSION_AUTHOR);
+	cerver_log_msg ("\n\nTiny Things Version: %s\n", THINGS_VERSION_NAME);
+	cerver_log_msg ("Release Date & time: %s - %s\n", THINGS_VERSION_DATE, THINGS_VERSION_TIME);
+	cerver_log_msg ("Author: %s\n\n", THINGS_VERSION_AUTHOR);
 
 }
 
 // print the version id
 void things_version_print_version_id (void) {
 
-	printf ("\n\nTiny Things Version ID: %s\n", THINGS_VERSION);
+	cerver_log_msg ("\n\nTiny Things Version ID: %s\n", THINGS_VERSION);
 
 }
 
 // print the version name
 void things_version_print_version_name (void) {
 
-	printf ("\n\nTiny Things Version: %s\n", THINGS_VERSION_NAME);
+	cerver_log_msg ("\n\nTiny Things Version: %s\n", THINGS_VERSION_NAME);
 
 }
