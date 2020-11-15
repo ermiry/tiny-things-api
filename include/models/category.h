ccdc6923525586dbc8dbf9a78aac940bfb74d4fa
ermiry <erick_salas@ermiry.com>
2020-11-14 21:12:12 -0600

    Added query & bson related methods to category model

--- include/models/category.h
+++ include/models/category.h
@@ -37,4 +37,34 @@ extern void category_delete (void *category_ptr);
 
 extern void category_print (Category *category);
 
+extern bson_t *category_query_oid (const bson_oid_t *oid);
+
+extern const bson_t *category_find_by_oid (
+	const bson_oid_t *oid, const bson_t *query_opts
+);
+
+extern u8 category_get_by_oid (
+	Category *category, const bson_oid_t *oid, const bson_t *query_opts
+);
+
+extern const bson_t *category_find_by_oid_and_user (
+	const bson_oid_t *oid, const bson_oid_t *user_oid,
+	const bson_t *query_opts
+);
+
+extern u8 category_get_by_oid_and_user (
+	Category *category,
+	const bson_oid_t *oid, const bson_oid_t *user_oid,
+	const bson_t *query_opts
+);
+
+extern bson_t *category_to_bson (Category *category);
+
+extern bson_t *category_update_bson (Category *category);
+
+// get all the categories that are related to a user
+extern mongoc_cursor_t *categories_get_all_by_user (
+	const bson_oid_t *user_oid, const bson_t *opts
+);
+
 #endif
--- src/models/category.c
+++ src/models/category.c
@@ -72,3 +72,188 @@ void category_print (Category *category) {
 	}
 
 }
+
+static void category_doc_parse (Category *category, const bson_t *category_doc) {
+
+	bson_iter_t iter = { 0 };
+	if (bson_iter_init (&iter, category_doc)) {
+		char *key = NULL;
+		bson_value_t *value = NULL;
+		while (bson_iter_next (&iter)) {
+			key = (char *) bson_iter_key (&iter);
+			value = (bson_value_t *) bson_iter_value (&iter);
+
+			if (!strcmp (key, "_id"))
+				bson_oid_copy (&value->value.v_oid, &category->oid);
+
+			else if (!strcmp (key, "user"))
+				bson_oid_copy (&value->value.v_oid, &category->user_oid);
+
+			else if (!strcmp (key, "title") && value->value.v_utf8.str) 
+				(void) strncpy (category->title, value->value.v_utf8.str, CATEGORY_TITLE_LEN);
+
+			else if (!strcmp (key, "description")) 
+				(void) strncpy (category->description, value->value.v_utf8.str, CATEGORY_DESCRIPTION_LEN);
+
+			else if (!strcmp (key, "date")) 
+				category->date = (time_t) bson_iter_date_time (&iter) / 1000;
+		}
+	}
+
+}
+
+bson_t *category_query_oid (const bson_oid_t *oid) {
+
+	bson_t *query = NULL;
+
+	if (oid) {
+		query = bson_new ();
+		if (query) {
+			(void) bson_append_oid (query, "_id", -1, oid);
+		}
+	}
+
+	return query;
+
+}
+
+const bson_t *category_find_by_oid (
+	const bson_oid_t *oid, const bson_t *query_opts
+) {
+
+	const bson_t *retval = NULL;
+
+	bson_t *category_query = bson_new ();
+	if (category_query) {
+		(void) bson_append_oid (category_query, "_id", -1, oid);
+		retval = mongo_find_one_with_opts (categories_collection, category_query, query_opts);
+	}
+
+	return retval;
+
+}
+
+u8 category_get_by_oid (
+	Category *category, const bson_oid_t *oid, const bson_t *query_opts
+) {
+
+	u8 retval = 1;
+
+	if (category) {
+		const bson_t *category_doc = category_find_by_oid (oid, query_opts);
+		if (category_doc) {
+			category_doc_parse (category, category_doc);
+			bson_destroy ((bson_t *) category_doc);
+
+			retval = 0;
+		}
+	}
+
+	return retval;
+
+}
+
+const bson_t *category_find_by_oid_and_user (
+	const bson_oid_t *oid, const bson_oid_t *user_oid,
+	const bson_t *query_opts
+) {
+
+	const bson_t *retval = NULL;
+
+	bson_t *category_query = bson_new ();
+	if (category_query) {
+		(void) bson_append_oid (category_query, "_id", -1, oid);
+		(void) bson_append_oid (category_query, "user", -1, user_oid);
+
+		retval = mongo_find_one_with_opts (categories_collection, category_query, query_opts);
+	}
+
+	return retval;
+
+}
+
+u8 category_get_by_oid_and_user (
+	Category *category,
+	const bson_oid_t *oid, const bson_oid_t *user_oid,
+	const bson_t *query_opts
+) {
+
+	u8 retval = 1;
+
+	if (category) {
+		const bson_t *category_doc = category_find_by_oid_and_user (oid, user_oid, query_opts);
+		if (category_doc) {
+			category_doc_parse (category, category_doc);
+			bson_destroy ((bson_t *) category_doc);
+
+			retval = 0;
+		}
+	}
+
+	return retval;
+
+}
+
+bson_t *category_to_bson (Category *category) {
+
+    bson_t *doc = NULL;
+
+    if (category) {
+        doc = bson_new ();
+        if (doc) {
+            (void) bson_append_oid (doc, "_id", -1, &category->oid);
+
+			(void) bson_append_oid (doc, "user", -1, &category->user_oid);
+
+			(void) bson_append_utf8 (doc, "title", -1, category->title, -1);
+			(void) bson_append_utf8 (doc, "description", -1, category->description, -1);
+
+			(void) bson_append_date_time (doc, "date", -1, category->date * 1000);
+        }
+    }
+
+    return doc;
+
+}
+
+bson_t *category_update_bson (Category *category) {
+
+	bson_t *doc = NULL;
+
+    if (category) {
+        doc = bson_new ();
+        if (doc) {
+			bson_t set_doc = { 0 };
+			(void) bson_append_document_begin (doc, "$set", -1, &set_doc);
+			(void) bson_append_utf8 (&set_doc, "title", -1, category->title, -1);
+			(void) bson_append_utf8 (&set_doc, "description", -1, category->description, -1);
+			(void) bson_append_document_end (doc, &set_doc);
+        }
+    }
+
+    return doc;
+
+}
+
+// get all the categories that are related to a user
+mongoc_cursor_t *categories_get_all_by_user (
+	const bson_oid_t *user_oid, const bson_t *opts
+) {
+
+	mongoc_cursor_t *retval = NULL;
+
+	if (user_oid && opts) {
+		bson_t *query = bson_new ();
+		if (query) {
+			(void) bson_append_oid (query, "user", -1, user_oid);
+
+			retval = mongo_find_all_cursor_with_opts (
+				categories_collection,
+				query, opts
+			);
+		}
+	}
+
+	return retval;
+
+}
