eb02f6c3ea32e35312cf39064ca1012365cb4648
ermiry <erick.salas@ermiry.com>
2021-04-01 23:55:24 -0600

    Refactored version logging methods

--- src/version.c
+++ src/version.c
@@ -5,22 +5,39 @@
 // print full things version information
 void things_version_print_full (void) {
 
-	cerver_log_msg ("\n\nTiny Things Version: %s\n", THINGS_VERSION_NAME);
-	cerver_log_msg ("Release Date & time: %s - %s\n", THINGS_VERSION_DATE, THINGS_VERSION_TIME);
-	cerver_log_msg ("Author: %s\n\n", THINGS_VERSION_AUTHOR);
+	cerver_log_both (
+		LOG_TYPE_NONE, LOG_TYPE_NONE,
+		"Tiny Things Version: %s", THINGS_VERSION_NAME
+	);
+
+	cerver_log_both (
+		LOG_TYPE_NONE, LOG_TYPE_NONE,
+		"Release Date & time: %s - %s", THINGS_VERSION_DATE, THINGS_VERSION_TIME
+	);
+
+	cerver_log_both (
+		LOG_TYPE_NONE, LOG_TYPE_NONE,
+		"Author: %s\n", THINGS_VERSION_AUTHOR
+	);
 
 }
 
 // print the version id
 void things_version_print_version_id (void) {
 
-	cerver_log_msg ("\n\nTiny Things Version ID: %s\n", THINGS_VERSION);
+	cerver_log_both (
+		LOG_TYPE_NONE, LOG_TYPE_NONE,
+		"Tiny Things Version ID: %s", THINGS_VERSION
+	);
 
 }
 
 // print the version name
 void things_version_print_version_name (void) {
 
-	cerver_log_msg ("\n\nTiny Things Version: %s\n", THINGS_VERSION_NAME);
+	cerver_log_both (
+		LOG_TYPE_NONE, LOG_TYPE_NONE,
+		"Tiny Things Version: %s", THINGS_VERSION_NAME
+	);
 
 }
