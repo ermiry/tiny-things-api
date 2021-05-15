d346eb6f13bac0a49ff3bf71a7dfab37aff848ca
ermiry <erick.salas@ermiry.com>
2021-05-14 19:30:06 -0500

    Added more internal methods in label model

--- include/models/label.h
+++ include/models/label.h
@@ -3,34 +3,45 @@
 
 #include <time.h>
 
-#include <mongoc/mongoc.h>
 #include <bson/bson.h>
+#include <mongoc/mongoc.h>
 
 #include <cerver/types/types.h>
 
-#define LABEL_TITLE_LEN			    	512
-#define LABEL_DESCRIPTION_LEN			1024
-#define LABEL_COLOR_LEN					64
+#define LABELS_COLL_NAME        	"labels"
 
-extern mongoc_collection_t *labels_collection;
+#define LABEL_ID_SIZE				32
+#define LABEL_TITLE_SIZE			1024
+#define LABEL_DESCRIPTION_SIZE		2048
+#define LABEL_COLOR_SIZE			128
 
-// opens handle to labels collection
-extern unsigned int labels_collection_get (void);
+extern unsigned int labels_model_init (void);
 
-extern void labels_collection_close (void);
+extern void labels_model_end (void);
 
 typedef struct Label {
 
+	// label's unique id
 	bson_oid_t oid;
+	char id[LABEL_ID_SIZE];
 
+	// reference to the owner of this label
 	bson_oid_t user_oid;
 
 	bson_oid_t category_oid;
 
-	char title[LABEL_TITLE_LEN];
-	char description[LABEL_DESCRIPTION_LEN];
-	char color[LABEL_COLOR_LEN];
+	// how the user defined this transaction
+	char title[LABEL_TITLE_SIZE];
 
+	// a description added by the user to give extra information
+	char description[LABEL_DESCRIPTION_SIZE];
+
+	// the user can select a color
+	// that will be used to display all matching transactions
+	// in the mobile app
+	char color[LABEL_COLOR_SIZE];
+
+	// the date when the label was created
 	time_t date;
 
 } Label;
@@ -43,17 +54,12 @@ extern void label_print (Label *label);
 
 extern bson_t *label_query_oid (const bson_oid_t *oid);
 
-extern const bson_t *label_find_by_oid (
-	const bson_oid_t *oid, const bson_t *query_opts
+extern bson_t *label_query_by_oid_and_user (
+	const bson_oid_t *oid, const bson_oid_t *user_oid
 );
 
 extern u8 label_get_by_oid (
 	Label *label, const bson_oid_t *oid, const bson_t *query_opts
-);
-
-extern const bson_t *label_find_by_oid_and_user (
-	const bson_oid_t *oid, const bson_oid_t *user_oid,
-	const bson_t *query_opts
 );
 
 extern u8 label_get_by_oid_and_user (
@@ -62,13 +68,28 @@ extern u8 label_get_by_oid_and_user (
 	const bson_t *query_opts
 );
 
-extern bson_t *label_to_bson (Label *label);
-
-extern bson_t *label_update_bson (Label *label);
+extern u8 label_get_by_oid_and_user_to_json (
+	const bson_oid_t *oid, const bson_oid_t *user_oid,
+	const bson_t *query_opts,
+	char **json, size_t *json_len
+);
 
 // get all the labels that are related to a user
 extern mongoc_cursor_t *labels_get_all_by_user (
 	const bson_oid_t *user_oid, const bson_t *opts
+);
+
+extern unsigned int labels_get_all_by_user_to_json (
+	const bson_oid_t *user_oid, const bson_t *opts,
+	char **json, size_t *json_len
+);
+
+extern unsigned int label_insert_one (const Label *label);
+
+extern unsigned int label_update_one (const Label *label);
+
+extern unsigned int label_delete_one_by_oid_and_user (
+	const bson_oid_t *oid, const bson_oid_t *user_oid
 );
 
 #endif
--- src/models/label.c
+++ src/models/label.c
@@ -7,36 +7,36 @@
 
 #include <cerver/utils/log.h>
 
-#include "mongo.h"
+#include <cmongo/collections.h>
+#include <cmongo/crud.h>
+#include <cmongo/model.h>
 
 #include "models/label.h"
 
-#define LABELS_COLL_NAME         				"labels"
+static CMongoModel *labels_model = NULL;
 
-mongoc_collection_t *labels_collection = NULL;
+static void label_doc_parse (
+	void *label_ptr, const bson_t *label_doc
+);
 
-// opens handle to labels collection
-unsigned int labels_collection_get (void) {
+unsigned int labels_model_init (void) {
 
 	unsigned int retval = 1;
 
-	labels_collection = mongo_collection_get (LABELS_COLL_NAME);
-	if (labels_collection) {
-		cerver_log_debug ("Opened handle to labels collection!");
-		retval = 0;
-	}
+	labels_model = cmongo_model_create (LABELS_COLL_NAME);
+	if (labels_model) {
+		cmongo_model_set_parser (labels_model, label_doc_parse);
 
-	else {
-		cerver_log_error ("Failed to get handle to labels collection!");
+		retval = 0;
 	}
 
 	return retval;
 
 }
 
-void labels_collection_close (void) {
+void labels_model_end (void) {
 
-	if (labels_collection) mongoc_collection_destroy (labels_collection);
+	cmongo_model_delete (labels_model);
 
 }
 
@@ -64,9 +64,6 @@ void label_print (Label *label) {
 		bson_oid_to_string (&label->oid, buffer);
 		(void) printf ("id: %s\n", buffer);
 
-		bson_oid_to_string (&label->category_oid, buffer);
-		(void) printf ("label: %s\n", buffer);
-
 		(void) printf ("title: %s\n", label->title);
 		(void) printf ("description: %s\n", label->description);
 		(void) printf ("color: %s\n", label->color);
@@ -77,7 +74,11 @@ void label_print (Label *label) {
 
 }
 
-static void label_doc_parse (Label *label, const bson_t *label_doc) {
+static void label_doc_parse (
+	void *label_ptr, const bson_t *label_doc
+) {
+
+	Label *label = (Label *) label_ptr;
 
 	bson_iter_t iter = { 0 };
 	if (bson_iter_init (&iter, label_doc)) {
@@ -87,26 +88,46 @@ static void label_doc_parse (Label *label, const bson_t *label_doc) {
 			key = (char *) bson_iter_key (&iter);
 			value = (bson_value_t *) bson_iter_value (&iter);
 
-			if (!strcmp (key, "_id"))
+			if (!strcmp (key, "_id")) {
 				bson_oid_copy (&value->value.v_oid, &label->oid);
+				bson_oid_to_string (&label->oid, label->id);
+			}
 
-			else if (!strcmp (key, "user"))
+			else if (!strcmp (key, "user")) {
 				bson_oid_copy (&value->value.v_oid, &label->user_oid);
+			}
 
-			else if (!strcmp (key, "category"))
+			else if (!strcmp (key, "category")) {
 				bson_oid_copy (&value->value.v_oid, &label->category_oid);
+			}
 
-			else if (!strcmp (key, "title") && value->value.v_utf8.str) 
-				(void) strncpy (label->title, value->value.v_utf8.str, LABEL_TITLE_LEN);
+			else if (!strcmp (key, "title") && value->value.v_utf8.str) {
+				(void) strncpy (
+					label->title,
+					value->value.v_utf8.str,
+					LABEL_TITLE_SIZE - 1
+				);
+			}
 
-			else if (!strcmp (key, "description")) 
-				(void) strncpy (label->description, value->value.v_utf8.str, LABEL_DESCRIPTION_LEN);
+			else if (!strcmp (key, "description")) {
+				(void) strncpy (
+					label->description,
+					value->value.v_utf8.str,
+					LABEL_DESCRIPTION_SIZE - 1
+				);
+			}
 
-			else if (!strcmp (key, "color")) 
-				(void) strncpy (label->color, value->value.v_utf8.str, LABEL_COLOR_LEN);
+			else if (!strcmp (key, "color")) {
+				(void) strncpy (
+					label->color,
+					value->value.v_utf8.str,
+					LABEL_COLOR_SIZE - 1
+				);
+			}
 
-			else if (!strcmp (key, "date")) 
+			else if (!strcmp (key, "date")) {
 				label->date = (time_t) bson_iter_date_time (&iter) / 1000;
+			}
 		}
 	}
 
@@ -127,19 +148,17 @@ bson_t *label_query_oid (const bson_oid_t *oid) {
 
 }
 
-const bson_t *label_find_by_oid (
-	const bson_oid_t *oid, const bson_t *query_opts
+bson_t *label_query_by_oid_and_user (
+	const bson_oid_t *oid, const bson_oid_t *user_oid
 ) {
-
-	const bson_t *retval = NULL;
 
 	bson_t *label_query = bson_new ();
 	if (label_query) {
 		(void) bson_append_oid (label_query, "_id", -1, oid);
-		retval = mongo_find_one_with_opts (labels_collection, label_query, query_opts);
+		(void) bson_append_oid (label_query, "user", -1, user_oid);
 	}
 
-	return retval;
+	return label_query;
 
 }
 
@@ -149,33 +168,16 @@ u8 label_get_by_oid (
 
 	u8 retval = 1;
 
-	if (label) {
-		const bson_t *label_doc = label_find_by_oid (oid, query_opts);
-		if (label_doc) {
-			label_doc_parse (label, label_doc);
-			bson_destroy ((bson_t *) label_doc);
-
-			retval = 0;
+	if (label && oid) {
+		bson_t *label_query = bson_new ();
+		if (label_query) {
+			(void) bson_append_oid (label_query, "_id", -1, oid);
+			retval = mongo_find_one_with_opts (
+				labels_model,
+				label_query, query_opts,
+				label
+			);
 		}
-	}
-
-	return retval;
-
-}
-
-const bson_t *label_find_by_oid_and_user (
-	const bson_oid_t *oid, const bson_oid_t *user_oid,
-	const bson_t *query_opts
-) {
-
-	const bson_t *retval = NULL;
-
-	bson_t *label_query = bson_new ();
-	if (label_query) {
-		(void) bson_append_oid (label_query, "_id", -1, oid);
-		(void) bson_append_oid (label_query, "user", -1, user_oid);
-
-		retval = mongo_find_one_with_opts (labels_collection, label_query, query_opts);
 	}
 
 	return retval;
@@ -190,13 +192,17 @@ u8 label_get_by_oid_and_user (
 
 	u8 retval = 1;
 
-	if (label) {
-		const bson_t *label_doc = label_find_by_oid_and_user (oid, user_oid, query_opts);
-		if (label_doc) {
-			label_doc_parse (label, label_doc);
-			bson_destroy ((bson_t *) label_doc);
+	if (label && oid && user_oid) {
+		bson_t *label_query = label_query_by_oid_and_user (
+			oid, user_oid
+		);
 
-			retval = 0;
+		if (label_query) {
+			retval = mongo_find_one_with_opts (
+				labels_model,
+				label_query, query_opts,
+				label
+			);
 		}
 	}
 
@@ -204,14 +210,40 @@ u8 label_get_by_oid_and_user (
 
 }
 
-bson_t *label_to_bson (Label *label) {
+u8 label_get_by_oid_and_user_to_json (
+	const bson_oid_t *oid, const bson_oid_t *user_oid,
+	const bson_t *query_opts,
+	char **json, size_t *json_len
+) {
 
-    bson_t *doc = NULL;
+	u8 retval = 1;
 
-    if (label) {
-        doc = bson_new ();
-        if (doc) {
-            (void) bson_append_oid (doc, "_id", -1, &label->oid);
+	if (oid && user_oid) {
+		bson_t *label_query = label_query_by_oid_and_user (
+			oid, user_oid
+		);
+
+		if (label_query) {
+			retval = mongo_find_one_with_opts_to_json (
+				labels_model,
+				label_query, query_opts,
+				json, json_len
+			);
+		}
+	}
+
+	return retval;
+
+}
+
+static bson_t *label_to_bson (const Label *label) {
+
+	bson_t *doc = NULL;
+
+	if (label) {
+		doc = bson_new ();
+		if (doc) {
+			(void) bson_append_oid (doc, "_id", -1, &label->oid);
 
 			(void) bson_append_oid (doc, "user", -1, &label->user_oid);
 
@@ -220,30 +252,30 @@ bson_t *label_to_bson (Label *label) {
 			(void) bson_append_utf8 (doc, "color", -1, label->color, -1);
 
 			(void) bson_append_date_time (doc, "date", -1, label->date * 1000);
-        }
-    }
+		}
+	}
 
-    return doc;
+	return doc;
 
 }
 
-bson_t *label_update_bson (Label *label) {
+static bson_t *label_update_bson (const Label *label) {
 
 	bson_t *doc = NULL;
 
-    if (label) {
-        doc = bson_new ();
-        if (doc) {
-			bson_t set_doc = { 0 };
+	if (label) {
+		doc = bson_new ();
+		if (doc) {
+			bson_t set_doc = BSON_INITIALIZER;
 			(void) bson_append_document_begin (doc, "$set", -1, &set_doc);
 			(void) bson_append_utf8 (&set_doc, "title", -1, label->title, -1);
 			(void) bson_append_utf8 (&set_doc, "description", -1, label->description, -1);
 			(void) bson_append_utf8 (&set_doc, "color", -1, label->color, -1);
 			(void) bson_append_document_end (doc, &set_doc);
-        }
-    }
+		}
+	}
 
-    return doc;
+	return doc;
 
 }
 
@@ -260,8 +292,73 @@ mongoc_cursor_t *labels_get_all_by_user (
 			(void) bson_append_oid (query, "user", -1, user_oid);
 
 			retval = mongo_find_all_cursor_with_opts (
-				labels_collection,
+				labels_model,
 				query, opts
+			);
+		}
+	}
+
+	return retval;
+
+}
+
+unsigned int labels_get_all_by_user_to_json (
+	const bson_oid_t *user_oid, const bson_t *opts,
+	char **json, size_t *json_len
+) {
+
+	unsigned int retval = 1;
+
+	if (user_oid) {
+		bson_t *query = bson_new ();
+		if (query) {
+			(void) bson_append_oid (query, "user", -1, user_oid);
+
+			retval = mongo_find_all_to_json (
+				labels_model,
+				query, opts,
+				"labels",
+				json, json_len
+			);
+		}
+	}
+
+	return retval;
+
+}
+
+unsigned int label_insert_one (const Label *label) {
+
+	return mongo_insert_one (
+		labels_model, label_to_bson (label)
+	);
+
+}
+
+unsigned int label_update_one (const Label *label) {
+
+	return mongo_update_one (
+		labels_model,
+		label_query_oid (&label->oid),
+		label_update_bson (label)
+	);
+
+}
+
+unsigned int label_delete_one_by_oid_and_user (
+	const bson_oid_t *oid, const bson_oid_t *user_oid
+) {
+
+	unsigned int retval = 1;
+
+	if (oid && user_oid) {
+		bson_t *label_query = label_query_by_oid_and_user (
+			oid, user_oid
+		);
+
+		if (label_query) {
+			retval = mongo_delete_one (
+				labels_model, label_query
 			);
 		}
 	}
