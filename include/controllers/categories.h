0703fe75e78cc0a116407c061a2a6efefe1cd8ea
ermiry <erick.salas@ermiry.com>
2021-05-15 23:23:08 -0500

    Added more categories controller methods

--- include/controllers/categories.h
+++ include/controllers/categories.h
@@ -5,11 +5,16 @@
 
 #include <cerver/types/string.h>
 
+#include "errors.h"
+
 #include "models/category.h"
+#include "models/user.h"
 
 #define DEFAULT_CATEGORIES_POOL_INIT			32
 
 struct _HttpResponse;
+
+extern const bson_t *category_no_user_query_opts;
 
 extern struct _HttpResponse *no_user_categories;
 extern struct _HttpResponse *no_user_category;
@@ -22,13 +27,34 @@ extern unsigned int things_categories_init (void);
 
 extern void things_categories_end (void);
 
+extern Category *things_category_get (void);
+
+extern unsigned int things_categories_get_all_by_user (
+	const bson_oid_t *user_oid,
+	char **json, size_t *json_len
+);
+
 extern Category *things_category_get_by_id_and_user (
 	const String *category_id, const bson_oid_t *user_oid
 );
 
-extern Category *things_category_create (
-	const char *user_id,
-	const char *title, const char *description
+extern u8 things_category_get_by_id_and_user_to_json (
+	const char *category_id, const bson_oid_t *user_oid,
+	const bson_t *query_opts,
+	char **json, size_t *json_len
+);
+
+extern ThingsError things_category_create (
+	const User *user, const String *request_body
+);
+
+extern ThingsError things_category_update (
+	const User *user, const String *category_id,
+	const String *request_body
+);
+
+extern ThingsError things_category_delete (
+	const User *user, const String *category_id
 );
 
 extern void things_category_return (void *category_ptr);
--- src/controllers/categories.c
+++ src/controllers/categories.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <string.h>
 
 #include <time.h>
 
@@ -11,23 +12,30 @@
 
 #include <cerver/utils/log.h>
 
+#include <cmongo/crud.h>
+#include <cmongo/select.h>
+
+#include "errors.h"
+
 #include "models/category.h"
+#include "models/user.h"
 
 #include "controllers/categories.h"
 
 static Pool *categories_pool = NULL;
 
 const bson_t *category_no_user_query_opts = NULL;
-DoubleList *category_no_user_select = NULL;
+static CMongoSelect *category_no_user_select = NULL;
 
 HttpResponse *no_user_categories = NULL;
 HttpResponse *no_user_category = NULL;
+
 HttpResponse *category_created_success = NULL;
 HttpResponse *category_created_bad = NULL;
 HttpResponse *category_deleted_success = NULL;
 HttpResponse *category_deleted_bad = NULL;
 
-void things_category_delete (void *category_ptr);
+void things_category_return (void *category_ptr);
 
 static unsigned int things_categories_init_pool (void) {
 
@@ -58,10 +66,11 @@ static unsigned int things_categories_init_query_opts (void) {
 
 	unsigned int retval = 1;
 
-	category_no_user_select = dlist_init (str_delete, str_comparator);
-	(void) dlist_insert_after (category_no_user_select, dlist_end (category_no_user_select), str_new ("title"));
-	(void) dlist_insert_after (category_no_user_select, dlist_end (category_no_user_select), str_new ("amount"));
-	(void) dlist_insert_after (category_no_user_select, dlist_end (category_no_user_select), str_new ("date"));
+	category_no_user_select = cmongo_select_new ();
+	(void) cmongo_select_insert_field (category_no_user_select, "title");
+	(void) cmongo_select_insert_field (category_no_user_select, "description");
+	(void) cmongo_select_insert_field (category_no_user_select, "color");
+	(void) cmongo_select_insert_field (category_no_user_select, "date");
 
 	category_no_user_query_opts = mongo_find_generate_opts (category_no_user_select);
 
@@ -76,27 +85,27 @@ static unsigned int things_categories_init_responses (void) {
 	unsigned int retval = 1;
 
 	no_user_categories = http_response_json_key_value (
-		(http_status) 404, "msg", "Failed to get user's categories"
+		HTTP_STATUS_NOT_FOUND, "msg", "Failed to get user's categories"
 	);
 
 	no_user_category = http_response_json_key_value (
-		(http_status) 404, "msg", "User's category was not found"
+		HTTP_STATUS_NOT_FOUND, "msg", "User's category was not found"
 	);
 
 	category_created_success = http_response_json_key_value (
-		(http_status) 200, "oki", "doki"
+		HTTP_STATUS_OK, "oki", "doki"
 	);
 
 	category_created_bad = http_response_json_key_value (
-		(http_status) 400, "error", "Failed to create category!"
+		HTTP_STATUS_BAD_REQUEST, "error", "Failed to create category!"
 	);
 
 	category_deleted_success = http_response_json_key_value (
-		(http_status) 200, "oki", "doki"
+		HTTP_STATUS_OK, "oki", "doki"
 	);
 
 	category_deleted_bad = http_response_json_key_value (
-		(http_status) 400, "error", "Failed to delete category!"
+		HTTP_STATUS_BAD_REQUEST, "error", "Failed to delete category!"
 	);
 
 	if (
@@ -125,10 +134,12 @@ unsigned int things_categories_init (void) {
 
 void things_categories_end (void) {
 
+	cmongo_select_delete (category_no_user_select);
 	bson_destroy ((bson_t *) category_no_user_query_opts);
 
 	http_response_delete (no_user_categories);
 	http_response_delete (no_user_category);
+
 	http_response_delete (category_created_success);
 	http_response_delete (category_created_bad);
 	http_response_delete (category_deleted_success);
@@ -136,6 +147,24 @@ void things_categories_end (void) {
 
 	pool_delete (categories_pool);
 	categories_pool = NULL;
+
+}
+
+Category *things_category_get (void) {
+
+	return (Category *) pool_pop (categories_pool);
+
+}
+
+unsigned int things_categories_get_all_by_user (
+	const bson_oid_t *user_oid,
+	char **json, size_t *json_len
+) {
+
+	return categories_get_all_by_user_to_json (
+		user_oid, category_no_user_query_opts,
+		json, json_len
+	);
 
 }
 
@@ -155,7 +184,7 @@ Category *things_category_get_by_id_and_user (
 				&category->oid, user_oid,
 				NULL
 			)) {
-				things_category_delete (category);
+				things_category_return (category);
 				category = NULL;
 			}
 		}
@@ -165,9 +194,33 @@ Category *things_category_get_by_id_and_user (
 
 }
 
-Category *things_category_create (
+u8 things_category_get_by_id_and_user_to_json (
+	const char *category_id, const bson_oid_t *user_oid,
+	const bson_t *query_opts,
+	char **json, size_t *json_len
+) {
+
+	u8 retval = 1;
+
+	if (category_id) {
+		bson_oid_t category_oid = { 0 };
+		bson_oid_init_from_string (&category_oid, category_id);
+
+		retval = category_get_by_oid_and_user_to_json (
+			&category_oid, user_oid,
+			query_opts,
+			json, json_len
+		);
+	}
+
+	return retval;
+
+}
+
+static Category *things_category_create_actual (
 	const char *user_id,
-	const char *title, const char *description
+	const char *title, const char *description,
+	const char *color
 ) {
 
 	Category *category = (Category *) pool_pop (categories_pool);
@@ -178,11 +231,254 @@ Category *things_category_create (
 
 		if (title) (void) strncpy (category->title, title, CATEGORY_TITLE_SIZE - 1);
 		if (description) (void) strncpy (category->description, description, CATEGORY_DESCRIPTION_SIZE - 1);
+
+		if (color) (void) strncpy (category->color, color, CATEGORY_COLOR_SIZE - 1);
 		
 		category->date = time (NULL);
 	}
 
 	return category;
+
+}
+
+static void things_category_parse_json (
+	json_t *json_body,
+	const char **title,
+	const char **description,
+	const char **color
+) {
+
+	// get values from json to create a new category
+	const char *key = NULL;
+	json_t *value = NULL;
+	if (json_typeof (json_body) == JSON_OBJECT) {
+		json_object_foreach (json_body, key, value) {
+			if (!strcmp (key, "title")) {
+				*title = json_string_value (value);
+				#ifdef THINGS_DEBUG
+				(void) printf ("title: \"%s\"\n", *title);
+				#endif
+			}
+
+			else if (!strcmp (key, "description")) {
+				*description = json_string_value (value);
+				#ifdef THINGS_DEBUG
+				(void) printf ("description: \"%s\"\n", *description);
+				#endif
+			}
+
+			else if (!strcmp (key, "color")) {
+				*color = json_string_value (value);
+				#ifdef THINGS_DEBUG
+				(void) printf ("color: \"%s\"\n", *color);
+				#endif
+			}
+		}
+	}
+
+}
+
+static ThingsError things_category_create_parse_json (
+	Category **category,
+	const char *user_id, const String *request_body
+) {
+
+	ThingsError error = THINGS_ERROR_NONE;
+
+	const char *title = NULL;
+	const char *description = NULL;
+	const char *color = NULL;
+
+	json_error_t json_error =  { 0 };
+	json_t *json_body = json_loads (request_body->str, 0, &json_error);
+	if (json_body) {
+		things_category_parse_json (
+			json_body,
+			&title,
+			&description,
+			&color
+		);
+
+		*category = things_category_create_actual (
+			user_id,
+			title, description,
+			color
+		);
+
+		json_decref (json_body);
+	}
+
+	else {
+		cerver_log_error (
+			"json_loads () - json error on line %d: %s\n", 
+			json_error.line, json_error.text
+		);
+
+		error = THINGS_ERROR_BAD_REQUEST;
+	}
+
+	return error;
+
+}
+
+ThingsError things_category_create (
+	const User *user, const String *request_body
+) {
+
+	ThingsError error = THINGS_ERROR_NONE;
+
+	if (request_body) {
+		Category *category = NULL;
+
+		error = things_category_create_parse_json (
+			&category,
+			user->id, request_body
+		);
+
+		if (error == THINGS_ERROR_NONE) {
+			#ifdef THINGS_DEBUG
+			category_print (category);
+			#endif
+
+			if (!category_insert_one (
+				category
+			)) {
+				// update users values
+				(void) user_add_category (user);
+			}
+
+			else {
+				error = THINGS_ERROR_SERVER_ERROR;
+			}
+			
+			things_category_return (category);
+		}
+	}
+
+	else {
+		#ifdef THINGS_DEBUG
+		cerver_log_error ("Missing request body to create category!");
+		#endif
+
+		error = THINGS_ERROR_BAD_REQUEST;
+	}
+
+	return error;
+
+}
+
+static ThingsError things_category_update_parse_json (
+	Category *category, const String *request_body
+) {
+
+	ThingsError error = THINGS_ERROR_NONE;
+
+	const char *title = NULL;
+	const char *description = NULL;
+	const char *color = NULL;
+
+	json_error_t json_error =  { 0 };
+	json_t *json_body = json_loads (request_body->str, 0, &json_error);
+	if (json_body) {
+		things_category_parse_json (
+			json_body,
+			&title,
+			&description,
+			&color
+		);
+
+		if (title) (void) strncpy (category->title, title, CATEGORY_TITLE_SIZE - 1);
+		if (description) (void) strncpy (category->description, description, CATEGORY_DESCRIPTION_SIZE - 1);
+		if (color) (void) strncpy (category->color, color, CATEGORY_COLOR_SIZE - 1);
+
+		json_decref (json_body);
+	}
+
+	else {
+		#ifdef THINGS_DEBUG
+		cerver_log_error (
+			"json_loads () - json error on line %d: %s\n", 
+			json_error.line, json_error.text
+		);
+		#endif
+
+		error = THINGS_ERROR_BAD_REQUEST;
+	}
+
+	return error;
+
+}
+
+ThingsError things_category_update (
+	const User *user, const String *category_id,
+	const String *request_body
+) {
+
+	ThingsError error = THINGS_ERROR_NONE;
+
+	if (request_body) {
+		Category *category = things_category_get_by_id_and_user (
+			category_id, &user->oid
+		);
+
+		if (category) {
+			// get update values
+			if (things_category_update_parse_json (
+				category, request_body
+			) == THINGS_ERROR_NONE) {
+				// update the category in the db
+				if (category_update_one (category)) {
+					error = THINGS_ERROR_SERVER_ERROR;
+				}
+			}
+
+			things_category_return (category);
+		}
+
+		else {
+			#ifdef THINGS_DEBUG
+			cerver_log_error ("Failed to get matching category!");
+			#endif
+
+			error = THINGS_ERROR_NOT_FOUND;
+		}
+	}
+
+	else {
+		#ifdef THINGS_DEBUG
+		cerver_log_error ("Missing request body to update category!");
+		#endif
+
+		error = THINGS_ERROR_BAD_REQUEST;
+	}
+
+	return error;
+
+}
+
+// TODO: handle things that reference the requested category
+ThingsError things_category_delete (
+	const User *user, const String *category_id
+) {
+
+	ThingsError error = THINGS_ERROR_NONE;
+
+	bson_oid_t oid = { 0 };
+	bson_oid_init_from_string (&oid, category_id->str);
+
+	if (!category_delete_one_by_oid_and_user (
+		&oid, &user->oid
+	)) {
+		#ifdef THINGS_DEBUG
+		cerver_log_debug ("Deleted category %s", category_id->str);
+		#endif
+	}
+
+	else {
+		error = THINGS_ERROR_BAD_REQUEST;
+	}
+
+	return error;
 
 }
 
